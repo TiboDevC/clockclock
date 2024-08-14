3d39066081f8be126959bf0e841ccce49f281352
TiboDevC <tibo.dev.iot@gmail.com>
2024-08-14 12:17:01 +0200

    [shift_register] Speed-up pin toggling
    digitalWrite was too slow, remove unnecessary instructions from
    this function

--- src/shift_register.c
+++ src/shift_register.c
@@ -52,17 +52,68 @@ void ctrl_test(void)
 	}
 }
 
-void ctrl_motors(const uint8_t *byte_array, int num_bits)
+static void _digitalWrite_data(uint8_t val)
 {
-	for (int bit_pos = 0; bit_pos < num_bits; bit_pos++) {
-		const int bit_value = byte_array[bit_pos / 8] >> (bit_pos % 8) & 0x01;
-		digitalWrite(PIN_SERIAL_DATA_OUTPUT, bit_value);
+	const uint8_t bit = digitalPinToBitMask(PIN_SERIAL_DATA_OUTPUT);
+	const uint8_t port = digitalPinToPort(PIN_SERIAL_DATA_OUTPUT);
+	volatile uint8_t *out;
 
-		digitalWrite(PIN_SHIFT_REGISTER_CLOCK, HIGH);
-		digitalWrite(PIN_SHIFT_REGISTER_CLOCK, LOW);
+	out = portOutputRegister(port);
+
+	uint8_t oldSREG = SREG;
+	cli();
+
+	if (val == LOW) {
+		*out &= ~bit;
+	} else {
+		*out |= bit;
+	}
+
+	SREG = oldSREG;
+}
+
+static void _shift_register_low()
+{
+	const uint8_t bit = digitalPinToBitMask(PIN_SHIFT_REGISTER_CLOCK);
+	const uint8_t port = digitalPinToPort(PIN_SHIFT_REGISTER_CLOCK);
+	volatile uint8_t *out = portOutputRegister(port);
+	const uint8_t oldSREG = SREG;
+
+	cli();
+	*out &= ~bit;
+
+	SREG = oldSREG;
+}
+
+static void _shift_register_high()
+{
+	const uint8_t bit = digitalPinToBitMask(PIN_SHIFT_REGISTER_CLOCK);
+	const uint8_t port = digitalPinToPort(PIN_SHIFT_REGISTER_CLOCK);
+	volatile uint8_t *out = portOutputRegister(port);
+	const uint8_t oldSREG = SREG;
+
+	cli();
+	*out |= bit;
+
+	SREG = oldSREG;
+}
+
+void ctrl_motors(const uint8_t *byte_array, int num_motors)
+{
+	uint8_t last_bit_value = 0;
+	for (int motor_idx = 0; motor_idx < num_motors; motor_idx++) {
+		for (int bit_id = 0; bit_id < 4; bit_id++) {
+			const uint8_t bit_value = byte_array[motor_idx] >> bit_id & 0x01;
+			if (last_bit_value != bit_value) {
+				_digitalWrite_data(bit_value);
+				last_bit_value = bit_value;
+			}
+		}
+
+		_shift_register_high();
+		_shift_register_low();
 	}
 
 	digitalWrite(PIN_STORAGE_REGISTER_CLOCK, HIGH);
 	digitalWrite(PIN_STORAGE_REGISTER_CLOCK, LOW);
-	delay(2);
 }
