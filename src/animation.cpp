ec3312d25f1bc3953a8655534b933670ef9fdbb0
TiboDevC <tibo.dev.iot@gmail.com>
2024-08-14 14:04:00 +0200

    [animation] Indent code

--- src/animation.cpp
+++ src/animation.cpp
@@ -3,15 +3,15 @@
 
 #include "shift_register.h"
 
-#define NUM_MOTORS           48
-#define NUM_STEPS_PER_ROT    (4096)
-#define DELAY_FACTOR         5
-#define DELAY_OFFSET         1500 /* min delay in micro second to switch to next motor sequence */
+#define NUM_MOTORS        48
+#define NUM_STEPS_PER_ROT (4096)
+#define DELAY_FACTOR      10
+#define DELAY_OFFSET      1500 /* min delay in micro second to switch to next motor sequence */
 #define ANGLE_TO_STEPS(target_angle) \
 	((uint16_t) (((uint32_t) target_angle * NUM_STEPS_PER_ROT) / (uint32_t) 360))
 #define DELAY_TO_US(delay) (((unsigned long) delay * DELAY_FACTOR) + DELAY_OFFSET)
 
-#define INITIAL_DELAY        200
+#define INITIAL_DELAY        255
 #define INITIAL_SPEED        200
 #define INITIAL_ACCELERATION 1
 
