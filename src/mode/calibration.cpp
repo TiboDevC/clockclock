a98cab3ee166d0e8acc545da991ef102c49db74c
TiboDevC <tibo.dev.iot@gmail.com>
2024-08-21 13:31:33 +0200

    [calib] Add debug logs

--- src/mode/calibration.cpp
+++ src/mode/calibration.cpp
@@ -1,8 +1,18 @@
 #include <stdint.h>
 
+#include <HardwareSerial.h>
+
 #include "button/button.hpp"
 #include "cfg.hpp"
 #include "motor/motor_motion.h"
+
+#ifdef DEBUG_CALIB
+#define DBG_CALIB(...)    Serial.print(__VA_ARGS__)
+#define DBG_CALIB_LN(...) Serial.println(__VA_ARGS__)
+#else
+#define DBG_CALIB(...)
+#define DBG_CALIB_LN(...)
+#endif
 
 static struct {
 	uint8_t motor_idx;
@@ -29,5 +39,10 @@ void loop_calib()
 	const int8_t increment_count = button_get_encoder_count();
 	if (0 != increment_count) {
 		increment_needle_pos(_ctx.motor_idx, increment_count);
+		DBG_CALIB("Increment ");
+		DBG_CALIB(_ctx.motor_idx);
+		DBG_CALIB(" of ");
+		DBG_CALIB(increment_count);
+		DBG_CALIB_LN("");
 	}
 }
