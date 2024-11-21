7348b6cadbd59c9162af2ed08be2aa91483c9741
TiboDevC <tibo.dev.iot@gmail.com>
2024-11-21 18:48:38 +0100

    [cfg] Indent code

--- include/cfg.hpp
+++ include/cfg.hpp
@@ -1,7 +1,8 @@
 #ifndef SOFT_CFG_HPP
 #define SOFT_CFG_HPP
 
-#define NUM_MOTORS     48
-#define SHIFT_REG_SIZE (NUM_MOTORS / 2)
+#define NUM_MOTORS        48
+#define SHIFT_REG_SIZE    (NUM_MOTORS / 2)
+#define NUM_STEPS_PER_ROT (4096 / 2)
 
 #endif /* SOFT_CFG_HPP */
