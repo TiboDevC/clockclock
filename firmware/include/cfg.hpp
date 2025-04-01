ed54567bea08363c917993ae0fb58eda897877a7
TiboDevC <tibo.dev.iot@gmail.com>
2025-04-01 19:32:50 +0200

    Update number of steps per slot
    New steppers BKA30D-R5

--- firmware/include/cfg.hpp
+++ firmware/include/cfg.hpp
@@ -5,6 +5,6 @@
 /* 1 shift register for 4 motors */
 #define NUM_MOTORS_PER_SHIFT_REG 4
 #define SHIFT_REG_SIZE           (NUM_MOTORS / NUM_MOTORS_PER_SHIFT_REG)
-#define NUM_STEPS_PER_ROT        (4096 / 2)
+#define NUM_STEPS_PER_ROT        (360 * 12) /* 12 steps per degree */
 
 #endif /* SOFT_CFG_HPP */
